562400cd5a9adf3d807b02f23b9a4aaba12bafc7
aniket4999 <35426883+aniket4999@users.noreply.github.com>
2024-06-24 22:24:07 +0530

    Update MainS3.tf

--- MainS3.tf
+++ MainS3.tf
@@ -1,7 +1,7 @@
-export AWS_ACCESS_KEY_ID="acessskey"
-export AWS_SECRET_ACCESS_KEY="secretkey"
 provider "aws" {
         region = "us-east-1"
+        access_key = "AKIA5FTZEN7XLR4GFMGD"
+        secret_key = "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"
 }
 
 resource "aws_s3_bucket" "TFbucketandy" {
