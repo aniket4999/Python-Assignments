01671da7760796d9d70e17e54d96cb12ffe9295c
aniket4999 <35426883+aniket4999@users.noreply.github.com>
2024-06-25 20:19:25 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-        region = "us-east-1"
-        access_key = "AKIA5FTZEN7XLR4GFMGD"
-        secret_key = "zz+EPXAlZPObN1cxCUeBxLySQGwu1FZ75bDFEP/I"
-}
 resource "aws_instance" "example_server" {
   ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
