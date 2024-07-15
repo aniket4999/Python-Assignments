f31388f9c6de95c7b5c35feb3a1eea50405cdbc0
aniket4999 <35426883+aniket4999@users.noreply.github.com>
2024-07-15 20:13:42 +0530

    Update hello.sh

--- Jenkins/hello.sh
+++ Jenkins/hello.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Hello World"
-temp_role=$(aws sts assume-role --role-arn "$arn:aws:iam::905418080828:user/Jenkin" --role-session-name "$testsession")
+temp_role=$(aws sts assume-role --role-arn "arn:aws:iam::905418080828:user/Jenkin" --role-session-name "testsession")
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
