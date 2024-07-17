1958f3784f7ea2a9ecdc64524d3e848fe918145f
aniket4999 <35426883+aniket4999@users.noreply.github.com>
2024-07-17 23:52:36 +0530

    Update hello.sh

--- Jenkins/hello.sh
+++ Jenkins/hello.sh
@@ -6,3 +6,4 @@ export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccess
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
 echo "$AWS_ACCESS_KEY_ID"
 echo "$AWS_SECRET_ACCESS_KEY"
+aws sts get-caller-identity
