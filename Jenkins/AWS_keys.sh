75da846288e23b3fa050ce70bc1fbf4eb9bb9f18
aniket4999 <35426883+aniket4999@users.noreply.github.com>
2024-07-23 22:27:08 +0530

    Create AWS_keys.sh

---
+++ Jenkins/AWS_keys.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Hello World"
+#AWS_ACCESS_KEY_ID=""
+#AWS_SECRET_ACCESS_KEY=""
+echo "------------------------------------------"
+aws sts get-caller-identity
+echo "------------------------------------------"
+temp_role=$(aws sts assume-role --role-arn arn:aws:iam::905418080828:role/EC2-Role  --role-session-name testsession)
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
+echo "$AWS_ACCESS_KEY_ID"
+echo "$AWS_SECRET_ACCESS_KEY"
+echo "$AWS_SESSION_TOKEN"
+#aws sts get-caller-identity
+echo "After assuming role"
