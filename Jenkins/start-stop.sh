c426038c617c25ebd8a9419d33922e0e2e9e71cb
aniket4999 <35426883+aniket4999@users.noreply.github.com>
2024-07-27 14:17:58 +0530

    Update start-stop.sh

--- Jenkins/start-stop.sh
+++ Jenkins/start-stop.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-temp=$(aws ec2 describe-instances     --query 'Reservations[0].Instances[0].state' \
---filters "Name=instance-type,Values=t2.micro" \
---output json)
-for i in $temp
+echo "Hello World"
+AWS_ACCESS_KEY_ID=""
+AWS_SECRET_ACCESS_KEY=""
+echo "------------------------------------------"
+aws sts get-caller-identity
+echo "------------------------------------------"
+temp_role=$(aws sts assume-role --role-arn arn:aws:iam::905418080828:role/24_EC2S3  --role-session-name testsession)
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
+echo "$AWS_ACCESS_KEY_ID"
+echo "$AWS_SECRET_ACCESS_KEY"
+echo "$AWS_SESSION_TOKEN"
+aws sts get-caller-identity
+#aws ec2 run-instances --image-id ami-0427090fd1714168b --count 2 --instance-type t2.micro --key-name key-02 --security-group-ids sg-06de595cfe17d5f71 --subnet-id subnet-054c4912158d85c1f
+int=$(aws ec2 describe-instances     --query 'Reservations[*].Instances[*].[InstanceId]' \
+--filters "Name=instance-state-name,Values=running" \
+--output text)
+echo "Instance-ID: $int"
+for i in $int
 do
-        if
-
-aws ec2 stop-instances \
-    --instance-ids
+    echo $i
+    if [ $i =  i-0db1de8af0c220a4e ]
+    then                        aws ec2 stop-instances --instance-ids $i
+    else
+            echo "Not in list"
+   fi
+done
